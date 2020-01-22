3c36d6a8af979ff88e90998aa4d4d9fbd086cc16
Barbara Carrijo <barbarahcarrijo@gmail.com>
2020-01-22 00:17:29 -0300

    feature[WIP]: Adds Devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
+  #devise_for :users
   resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # token auth routes available at /api/v1/auth
+  #namespace :api do
+  #  scope :v1 do
+  #    mount_devise_token_auth_for 'User', at: 'auth'
+  #  end
+  #end
+  #
+  ## devise_for :users
+  #devise_for :users, controllers: {
+  #    sessions:      'users/sessions',
+  #    registrations: 'users/registrations'
+  #}
+
+  scope :api, defaults: {format: :json} do
+    scope :v1, defaults: {format: :json} do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
+
+
 end
